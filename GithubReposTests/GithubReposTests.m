bb544c43f3c5bb15d5c0b2401dd65f748402d30b
LuizRamos <luizramospe@hotmail.com>
2019-05-16 12:39:03 -0400

    First Commmit

---
+++ GithubReposTests/GithubReposTests.m
@@ -0,0 +1,37 @@
+//
+//  GithubReposTests.m
+//  GithubReposTests
+//
+//  Created by Luiz on 5/16/19.
+//  Copyright © 2019 Luiz. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface GithubReposTests : XCTestCase
+
+@end
+
+@implementation GithubReposTests
+
+- (void)setUp {
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
