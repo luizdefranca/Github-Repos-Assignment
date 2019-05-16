7227775d8b69588cf2d4478ae4fff43a43ceb9c5
LuizRamos <luizramospe@hotmail.com>
2019-05-16 12:47:41 -0400

    Created story - Making the request

--- GithubRepos/ViewController.m
+++ GithubRepos/ViewController.m
@@ -16,7 +16,40 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+    NSURL *url = [NSURL URLWithString:@"https://api.github.com/users/lighthouse-labs/repos"]; // 1
+    NSURLRequest *urlRequest = [[NSURLRequest alloc] initWithURL:url]; // 2
+
+    NSURLSessionConfiguration *configuration = [NSURLSessionConfiguration defaultSessionConfiguration]; // 3
+    NSURLSession *session = [NSURLSession sessionWithConfiguration:configuration]; // 4
+
+    NSURLSessionDataTask *dataTask = [session dataTaskWithRequest:urlRequest completionHandler:^(NSData * _Nullable data, NSURLResponse * _Nullable response, NSError * _Nullable error) {
+
+        if (error) { // 1
+            // Handle the error
+            NSLog(@"error: %@", error.localizedDescription);
+            return;
+        }
+
+        NSError *jsonError = nil;
+        NSArray *repos = [NSJSONSerialization JSONObjectWithData:data options:0 error:&jsonError]; // 2
+
+        if (jsonError) { // 3
+            // Handle the error
+            NSLog(@"jsonError: %@", jsonError.localizedDescription);
+            return;
+        }
+
+        // If we reach this point, we have successfully retrieved the JSON from the API
+        for (NSDictionary *repo in repos) { // 4
+
+            NSString *repoName = repo[@"name"];
+            NSLog(@"repo: %@", repoName);
+        }
+
+    }]; // 5
+
+    [dataTask resume]; // 6
+
 }
 
 
